657632d247cee9bc3fe98aaa718336ff01090906
Bri <brianna.butler@duke.edu>
2020-08-18 15:29:06 -0400

    Begin

---
+++ LiveHive/LiveHive/LiveHive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveHive
+//
+//  Created by Brianna Butler on 8/18/20.
+//  Copyright © 2020 LiveHive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
